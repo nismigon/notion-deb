d9741a69e3a0bd063b58ada16e234d525a0d363a
nismigon <nismigon@protonmail.com>
2022-01-30 19:00:17 +0100

    Notion WebBrowser and builder

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# notion-deb
+# Notion Unofficial
+
+Written by Nismigon (<nismigon@protonmail.com>)
+
+This project has the aim to create an application for Linux of Notion. In order to this, this application uses electron framework to create an application and electron-builder to create a deb file.
---
+++ index.js
@@ -0,0 +1,11 @@
+const {app, BrowserWindow} = require("electron");
+
+app.on("ready", () => {
+    const browserWindow = new BrowserWindow({
+        autoHideMenuBar: true,
+        width : 800,
+        height : 600,
+        icon : "build/icons/icon.png"
+    });
+    browserWindow.loadURL("https://notion.so");
+})
