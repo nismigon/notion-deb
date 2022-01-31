75dcd97e75f1c1d185409b36b0fc0b7381a027ce
nismigon <nismigon@protonmail.com>
2022-01-31 10:15:36 +0100

    Maximize screen at startup

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const {app, BrowserWindow} = require("electron");
 app.on("ready", () => {
     const browserWindow = new BrowserWindow({
         autoHideMenuBar: true,
-        width : 800,
-        height : 600,
+        width: 800,
+        height: 600,
         icon : "build/icons/icon.png"
     });
+    browserWindow.maximize();
     browserWindow.loadURL("https://notion.so");
 })
